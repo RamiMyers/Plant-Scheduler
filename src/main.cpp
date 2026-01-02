4f6c5b3aa3f45b1117f299f7298833902af4cd57
Rami <myersrami17@gmail.com>
2026-01-01 18:38:57 -0600

    Started implementing state machine

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+// TODO: Verify state machine best practices
+
 #define SENSOR_PIN A0
 #define VALID_MAX 470
 #define VALID_MIN 0
@@ -13,8 +15,9 @@ const unsigned long T_budget = 5000;
 unsigned long now, nextRelease, sampleStart, sampleEnd, sampleTime, maxSampleTime = 0, lateness;
 unsigned deadlineMisses = 0, scheduleMissesThisCycle, scheduleMisses;
 unsigned moistureValue;
+byte state;
 
-void updateStateMachine(State state);
+void updateStateMachine();
 
 void setup() {
   Serial.begin(9600); 
@@ -24,46 +27,55 @@ void setup() {
 
 void loop() {
   now = micros();
+  updateStateMachine();
 
-  if (long(now - nextRelease) >= 0) {
-    lateness = now - nextRelease;
-    scheduleMissesThisCycle = lateness / T_sample;
-    scheduleMisses += scheduleMissesThisCycle;
-    nextRelease += (scheduleMissesThisCycle + 1) * T_sample;
+  Serial.print("Moisture Value: ");
+  Serial.println(moistureValue);
 
-    sampleStart = micros();
-    moistureValue = analogRead(SENSOR_PIN);
-    sampleEnd = micros();
-    sampleTime = sampleEnd - sampleStart;
+  Serial.print("Sample Time: ");
+  Serial.print(sampleTime);
+  Serial.println(" micros");
 
-    if (sampleTime > maxSampleTime) maxSampleTime = sampleTime;
-    if (sampleTime >= T_budget) deadlineMisses++;
+  Serial.print("Max Sample Time: ");
+  Serial.print(maxSampleTime);
+  Serial.println(" micros");
 
-    Serial.print("Moisture Value: ");
-    Serial.println(moistureValue);
-
-    Serial.print("Sample Time: ");
-    Serial.print(sampleTime);
-    Serial.println(" micros");
-
-    Serial.print("Max Sample Time: ");
-    Serial.print(maxSampleTime);
-    Serial.println(" micros");
-
-    Serial.print("Deadline Misses: ");
-    Serial.println(deadlineMisses);
+  Serial.print("Deadline Misses: ");
+  Serial.println(deadlineMisses);
 
 
-    Serial.print("Schedule Missed By: ");
-    Serial.print(lateness);
-    Serial.println(" micros");
+  Serial.print("Schedule Missed By: ");
+  Serial.print(lateness);
+  Serial.println(" micros");
 
-    Serial.print("Schedule Misses: ");
-    Serial.println(scheduleMisses);
-    Serial.println();
-  }  
+  Serial.print("Schedule Misses: ");
+  Serial.println(scheduleMisses);
+  Serial.println();
 }
 
-void updateStateMachine(State state) {
+void updateStateMachine() {
+  switch (state) {
+    case CHECK:
+      if (long(now - nextRelease) >= 0) {
+        lateness = now - nextRelease;
+        scheduleMissesThisCycle = lateness / T_sample;
+        scheduleMisses += scheduleMissesThisCycle;
+        nextRelease += (scheduleMissesThisCycle + 1) * T_sample;
+        state = WATERING;
+      }
 
+      break;
+    case WATERING:
+      sampleStart = micros();
+      moistureValue = analogRead(SENSOR_PIN);
+      sampleEnd = micros();
+      sampleTime = sampleEnd - sampleStart;
+
+      if (sampleTime > maxSampleTime) maxSampleTime = sampleTime;
+      if (sampleTime >= T_budget) deadlineMisses++;
+
+      state = CHECK;
+
+      break;
+  }
 }
